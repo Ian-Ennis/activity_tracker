820670f38046f2fe432c7ee393d6cab84d8118fb
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-24 11:04:20 -0700

    marc's code with a bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create 
         user = User.create(user_params)
         if user.valid?
             token = encode_token(user_id: user.id)
-            render json: { user: UserSerialier.new(user), jwt: token }, status: :created}
+            render json: {user: UserSerialier.new(user), jwt: token}, status: :created}
         else
             render json: {error: "Could not create user!"}, status: :unprocessable_entity
         end
+    end
 
+    def profile
+        render json: @user
     end
 
     private
 
-    der user_params
+    def user_params
         params.require(:user).permit(:username, :password, :email)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
-      # get: '/profile', 'users#profile' 
+      get '/profile', to: 'users#profile' 
     end
   end
-      
-  
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-
-  # ATTENTION! (below)
-  # I commented this out during Marc's review session (this line wasn't in his templates)
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
