42262fad033dba4a03f0c4e2346738a8e17c5caf
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-24 16:45:07 -0700

    updated POSTs for select menu, cleaned-up code

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,6 @@ import Select from "react-select";
 import MeditationForm from "./MeditationForm";
 import YogaForm from "./YogaForm";
 import CardioForm from "./CardioForm";
-import NewActivityForm from "./NewActivityForm";
 
 export default function Home({ header, activity, setActivity }) {
   const initialOptions = [
@@ -13,12 +12,9 @@ export default function Home({ header, activity, setActivity }) {
     { value: "create_new", label: "✏️ Add your own activity!" },
   ];
   const [activityOptions, setActivityOptions] = useState(initialOptions);
-  const [stateUpdater, setStateUpdater] = useState("");
-  // const [activityState, setActivityState] = useState('');
 
   function selectActivity(e) {
     console.log("inside update activity function");
-    console.log(e.value);
     setActivity(e.value);
   }
 
@@ -39,71 +35,59 @@ export default function Home({ header, activity, setActivity }) {
 
   function handleMeditationSubmit(e) {
     e.preventDefault();
-    console.log(e.target);
-    setStateUpdater(activity);
-    // console.log(stateUpdater)
+    const time = e.target.time.value;
+    const date = e.target.date.value;
 
-    // POST request
-    // const medSession = {
-    //   time,
-    //   date,
-    // };
-
-    // fetch("/meditation"),
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(medSession),
-    //   };
+    fetch("/meditation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        time: time,
+        date: date,
+      }),
+    });
   }
 
   function handleYogaSubmit(e) {
     e.preventDefault();
-    console.log(e.target);
-    // POST request
-    // const yogaSession = {
-    //   type,
-    //   time,
-    //   date,
-    // };
+    const time = e.target.time.value;
+    const type = e.target.type.value;
+    const date = e.target.date.value;
 
-    // fetch("/yoga"),
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(yogaSession),
-    //   };
+    fetch("/yoga", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        time: time,
+        type: type,
+        date: date
+      }),
+    });
   }
 
   function handleCardioSubmit(e) {
     e.preventDefault();
-    console.log(e.target);
-    // POST request
-    // const cardioSession = {
-    //   type,
-    //   distance,
-    //   time,
-    //   date,
-    // };
+    const type = e.target.type.value;
+    const distance = e.target.distance.value;
+    const time = e.target.time.value;
+    const date = e.target.date.value;
 
-    // fetch("/cardio"),
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(cardioSession),
-    //   };
-  }
-
-  function handleNewActivitySubmit(e) {
-    e.preventDefault();
-    console.log(e.target);
-    // POST request
+    fetch("/meditation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: type,
+        distance: distance,
+        time: time,
+        date: date
+      }),
+    });
   }
 
   return (
@@ -113,12 +97,12 @@ export default function Home({ header, activity, setActivity }) {
         <p>
           Down to meditate? Into yoga? Always on the move? Log your self-care
           sessions using the menu below, and keep track of your kick-ass
-          routines as you practice. Or, add a new routine entirely. You do you!
+          routines as you practice.
         </p>
       </div>
       <Select
-        className="activity_dropdown"
         autofocus
+        className="activity_dropdown"
         isSearchable
         placeholder="Select activity"
         onChange={selectActivity}
@@ -133,12 +117,13 @@ export default function Home({ header, activity, setActivity }) {
       {activity === "cardio" ? (
         <CardioForm handleCardioSubmit={handleCardioSubmit} />
       ) : null}
-      {activity === "create_new" ? (
-        <NewActivityForm handleNewActivitySubmit={handleNewActivitySubmit} />
-      ) : null}
     </div>
   );
 }
+
+
+
+
 
 // Learn how to do a POST on a submit event
 // For monday: learn how to do a fetch on a click event
@@ -166,3 +151,4 @@ export default function Home({ header, activity, setActivity }) {
 //           }
 //       }
 //   }
+
