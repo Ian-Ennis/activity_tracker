0ac52ae0c330035bd3d95491513c17a6ab1a46ec
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-26 16:12:24 -0700

    back end should be good to go

--- db/migrate/20220124202534_create_activities.rb
+++ db/migrate/20220124202534_create_activities.rb
@@ -2,6 +2,8 @@ class CreateActivities < ActiveRecord::Migration[6.1]
   def change
     create_table :activities do |t|
       t.string :name
+      t.integer :minutes
+      t.references :user
 
       t.timestamps
     end
