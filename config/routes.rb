ddae029a514032c5d7202df3896aaeca13edb8b1
Kristina Diaz <kristina.diaz84@gmail.com>
2022-01-25 18:26:17 -0700

    got signup & login working with API

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      get '/login', to: 'auth#create'
-      post '/profile', to: 'users#profile' 
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile' 
     end
   end
 
-  resources :meditations, only: [index, :create]
-  resources :yogas, only: [index, :create]
-  resources :cardios, only: [index, :create]
+  resources :meditations, only: [:index, :create]
+  resources :yogas, only: [:index, :create]
+  resources :cardios, only: [:index, :create]
 
 end
